511a5579f879bede7b73d4c4dea08ef7db92eb8d
vboparai <vboparai11@gmail.com>
2018-03-27 15:07:05 -0400

    Multple Activities Demo

---
+++ MultipleActivitiesDemo/app/src/main/java/com/example/v/multipleactivitiesdemo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.v.multipleactivitiesdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void toSecondActivity(View view){
+
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+
+        intent.putExtra("username", "rob");
+
+        startActivity(intent);
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
